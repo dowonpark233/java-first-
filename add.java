108aa339d5511fd694447c0e63ce03cb098a057a
dowonpark233 <ng233@naver.com>
2020-01-12 20:34:29 +0900

    2020-01-12

---
+++ add.java
@@ -0,0 +1,27 @@
+package test01;
+
+public class add {
+	public static void main(String[] args) {
+    int y;
+    int x;
+    int sum;
+    
+    y = 20000; x = 2000; 
+	sum = y + x;
+   System.out.print(sum
+		   );
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+}
+}
---
+++ minus.java
@@ -0,0 +1,11 @@
+package test01;
+
+public class minus {
+ public static void main(String[] args) {
+	 int A = 1;
+	 int B = 2;
+	 int minus = A - B;
+	 System.out.print(minus);
+	 
+}
+}
