b092113e83a8357543a07b824b39934488792e7f
dowonpark233 <58904546+dowonpark233@users.noreply.github.com>
2020-01-12 20:55:56 +0900

    Add files via upload

---
+++ division.java
@@ -0,0 +1,10 @@
+package test01;
+
+public class division {
+ public static void main(String[] args) {
+	 int A = 4;
+	 int B = 2;
+	 int division = A / B;
+			 System.out.print(division);
+}
+}
---
+++ multiply.java
@@ -0,0 +1,11 @@
+package test01;
+
+public class multiply {
+ public static void main(String[] args) {
+	 int A = 1;
+	 int B = 2;
+	 int multiply = A * B;
+		System.out.print(multiply);
+	 
+}
+}
